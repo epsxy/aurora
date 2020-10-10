f877581510fa61cdc080a3e51cb0db20f7ddf488
epsxy <epsxy@users.noreply.github.com>
2020-10-11 01:37:32 +0200

    feat(git): add git log commands

--- pkg/git/log.go
+++ pkg/git/log.go
@@ -1,14 +1,34 @@
 package git
 
 import (
+	"fmt"
 	"log"
 	"os/exec"
 )
 
-// GitLog runs `git log` command.
+// Log runs `git log --abrev-commit` command.
 // Exits if it fails.
-func GitLog() string {
+func Log() string {
 	cmd := exec.Command("git", "log", "--abbrev-commit")
+
+	out, err := cmd.Output()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return string(out)
+}
+
+//LogChangelog runs `git log --oneline --first-parent [rev1]...[rev2]`.
+//It logs the commit history between 2 revisions.
+//If the first one is empty, defaults to `HEAD`
+func LogChangelog(firstCommit string, secondCommit string) string {
+	if firstCommit == "" {
+		firstCommit = "HEAD"
+	}
+	opts := fmt.Sprintf("%s...%s", firstCommit, secondCommit)
+	cmd := exec.Command("git", "log", "--oneline", "--first-parent", opts)
 
 	out, err := cmd.Output()
 
