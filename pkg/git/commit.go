e652bb9cb3a6b69f2e7246851b3414ad41c7828c
epsxy <epsxy@users.noreply.github.com>
2020-10-11 03:04:08 +0200

    feat(git): add Commit model and parse Commit infos from string

--- pkg/git/commit.go
+++ pkg/git/commit.go
@@ -4,7 +4,18 @@ import (
 	"fmt"
 	"log"
 	"os/exec"
+	"strings"
 )
+
+//Commit struct respresents a git commit object
+type Commit struct {
+	Hash             string
+	Type             string
+	Scope            string
+	IsBreakingChange bool
+	IsMergeCommit    bool
+	Msg              string
+}
 
 // GitCommit runs `git commit -m` command.
 // Exits if it fails.
@@ -17,4 +28,52 @@ func GitCommit(m string) {
 	}
 
 	fmt.Printf("%s", out)
+}
+
+//FillFromStrMsg fill commit information from a string inside a git.Commit object
+func (c *Commit) FillFromStrMsg(m string) {
+	cType := ""
+	scope := ""
+	isBreakingChange := false
+	msg := ""
+
+	// FIXME: Dirty fix to identify default merge commits
+	if strings.Contains(m, "Merge pull request") {
+		(*c).Type = ""
+		(*c).Scope = ""
+		(*c).IsBreakingChange = false
+		(*c).IsMergeCommit = true
+		(*c).Msg = msg
+		return
+	}
+
+	// If `:` is missing, this commit does not respects conventional commits.
+	if !strings.Contains(m, ":") {
+		(*c).Type = "misc"
+		(*c).Scope = ""
+		(*c).IsBreakingChange = false
+		(*c).IsMergeCommit = false
+		(*c).Msg = m
+		return
+	}
+
+	globalSplit := strings.Split(m, ":")
+	funcPart := globalSplit[0]
+	msg = strings.Join(globalSplit[1:], "")
+	if strings.Contains(funcPart, "!") {
+		funcPart = strings.ReplaceAll(funcPart, "!", "")
+		isBreakingChange = true
+	}
+	cType = funcPart
+	if strings.Contains(funcPart, "(") {
+		scopePart := strings.Split(funcPart, "(")
+		cType = scopePart[0]
+		scope = strings.ReplaceAll(scopePart[1], ")", "")
+	}
+
+	(*c).Type = cType
+	(*c).Scope = scope
+	(*c).IsBreakingChange = isBreakingChange
+	(*c).IsMergeCommit = false
+	(*c).Msg = msg
 }
