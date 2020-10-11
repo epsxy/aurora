15349ebdcb37daca4ee9072e2fb0c85489bc6518
epsxy <epsxy@users.noreply.github.com>
2020-10-11 03:11:24 +0200

    feat: add changelog entrypoint and cmd

--- pkg/cmd/root.go
+++ pkg/cmd/root.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/epsxy/gommitizen/pkg/cli"
 	"github.com/epsxy/gommitizen/pkg/git"
+	"github.com/epsxy/gommitizen/pkg/prompt"
 	"github.com/spf13/cobra"
 )
 
@@ -25,9 +26,34 @@ var commit = &cobra.Command{
 	},
 }
 
+var changelog = &cobra.Command{
+	Use:   "changelog",
+	Short: "Run changelog generator",
+	Long: `
+Generate a changelog between now and a previous revision or branch.
+Supported synthax for example:
+	- gommitizen changelog v1.0.0
+	- gommitizen changelog feat-branch-1 master
+	- gommitizen changelog 0e4bc1a 7e1abc8
+	`,
+	Args: cobra.RangeArgs(1, 2),
+	Run: func(cmd *cobra.Command, args []string) {
+		f := ""
+		s := ""
+		if len(args) == 1 {
+			s = args[0]
+		}
+		if len(args) == 2 {
+			f = args[0]
+			s = args[1]
+		}
+		prompt.Changelog(f, s)
+	},
+}
+
 // Execute is the root entrypoint of the Cobra CLI
 func Execute() {
-	root.AddCommand(commit)
+	root.AddCommand(commit, changelog)
 	if err := root.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
