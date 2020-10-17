0ad8b182c3b787e5da1f093546a1b2fc8c9d5f8c
epsxy <epsxy@users.noreply.github.com>
2020-10-17 02:20:29 +0200

    feat(prompt): add colors and symbols for lint command

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/epsxy/gommitizen
 go 1.15
 
 require (
+	github.com/gookit/color v1.3.1
 	github.com/manifoldco/promptui v0.8.0
 	github.com/spf13/cobra v1.0.0
 	github.com/spf13/pflag v1.0.5 // indirect
--- pkg/prompt/linter.go
+++ pkg/prompt/linter.go
@@ -1,27 +1,32 @@
 package prompt
 
 import (
-	"fmt"
-	"log"
+	"os"
 
 	"github.com/epsxy/gommitizen/pkg/git"
+	"github.com/gookit/color"
 )
 
 //Lint displays linter prompt
 func Lint(firstCommit string, secondCommit string) {
 	fails := false
 	messages := git.LogCommitMessage(firstCommit, secondCommit)
+	successSymbol := "✔"
+	failSymbol := "✘"
 	for _, m := range messages {
 		if m != "" && !git.LintCommitMessage(m) {
 			if git.IsMergeCommit(m) {
+				color.Cyan.Printf("%s  %s\n", successSymbol, m)
 				continue
 			}
 			fails = true
-			fmt.Println("Invalid commit:")
-			fmt.Println(m)
+			color.Red.Printf("%s  %s\n", failSymbol, m)
+			continue
 		}
+		color.Cyan.Printf("%s  %s\n", successSymbol, m)
 	}
 	if fails {
-		log.Fatal("Some commits are not valid")
+		color.Red.Printf("Linting failed, some commits are not valid\n")
+		os.Exit(1)
 	}
 }
