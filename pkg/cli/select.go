d041b63e771ad435915754509c620cd547f4fe3e
epsxy <epsxy@users.noreply.github.com>
2022-04-04 23:35:35 +0200

    feat(cli): add search in Select prompt component

--- pkg/cli/select.go
+++ pkg/cli/select.go
@@ -2,16 +2,26 @@ package cli
 
 import (
 	"log"
+	"strings"
 
 	"github.com/manifoldco/promptui"
 )
 
 // Select display a select promptui item
 func Select(label string, items []string) string {
+	searcher := func(input string, index int) bool {
+		t := items[index]
+		name := strings.Replace(strings.ToLower(t), " ", "", -1)
+		input = strings.Replace(strings.ToLower(input), " ", "", -1)
+
+		return strings.Contains(name, input)
+	}
+
 	prompt := promptui.Select{
-		Label:  label,
-		Items:  items,
-		Stdout: &BellSkipper{},
+		Label:    label,
+		Items:    items,
+		Stdout:   &BellSkipper{},
+		Searcher: searcher,
 	}
 
 	_, res, err := prompt.Run()
