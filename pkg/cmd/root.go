6bada0705accf27b5c84070d4b775afdb5434414
epsxy <epsxy@users.noreply.github.com>
2022-04-06 00:36:23 +0200

    feat: handle aurora version in VERSION conf file

--- pkg/cmd/root.go
+++ pkg/cmd/root.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/spf13/cobra"
@@ -11,11 +12,19 @@ var root = &cobra.Command{
 	Use:     "aurora",
 	Short:   "Aurora is a commit formatter utility",
 	Long:    "A Go program to help you create conventional commits",
-	Version: "v0.2.1",
+	Version: "v0.0.0", // will be filled in `Execute` entrypoint
 }
 
 // Execute is the root entrypoint of the Cobra CLI
 func Execute() {
+	// init version number
+	version, err := os.ReadFile("VERSION")
+	if err != nil {
+		log.Fatal("unexpected error: impossible to find VERSION")
+	}
+	root.Version = string(version)
+
+	// add commands and flags
 	root.AddCommand(Commit, Changelog, Lint)
 	root.PersistentFlags().Bool("verbose", false, "enable verbose mode")
 
